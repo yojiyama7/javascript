40ebd6b57dba3a612daacc9e97fb43a793f27644
yayo256 <>
2018-09-20 18:19:37 +0900

    p5js

---
+++ processing/test_processing.js
@@ -0,0 +1,9 @@
+function setup() { // 最初に実行される関数
+	createCanvas(windowWidth, windowHeight); // canvasの作成
+}
+ 
+function draw() { // 毎フレーム実行される関数
+	//四角形
+	//rect(width/2, height/2, 50, 50, 5)
+	rect(width/2, height/2, 50, 50, 5, 20, 5, 20)
+}
